847d0673aed877401a42a55b7ad38f6c1632d03e
Alice <wicalice@gmail.com>
2019-05-30 15:38:30 -0400

    Trying to track Cozmo QR with World Position, not working

---
+++ gridworld_unity/Assets/CozmoGridPos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CozmoGridPos : MonoBehaviour
+{
+    public PopulateMap curr_map; 
+    public Tilemap curr_tilemap;
+    public Grid gridWorld; 
+    public GameObject qr_cozmo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("in Update\n"); 
+        gridWorld = transform.parent.GetComponent<Grid>();
+        Vector3Int cellPosition = gridWorld.WorldToCell(qr_cozmo.transform.position);
+        Debug.Log("Tile position is:\n");
+        Debug.Log(cellPosition.x);
+        Debug.Log(cellPosition.y); 
+        curr_map.MakeTile(cellPosition);   
+    }
+}
--- gridworld_unity/Assets/PopulateMap.cs
+++ gridworld_unity/Assets/PopulateMap.cs
@@ -14,23 +14,29 @@ public class PopulateMap : MonoBehaviour {
 
 	// Obtain JSON file 
 	void Awake(){
-			string filePath = "Items/Fire.json";
-			string LoadedItem = JsonFileReader.LoadJsonAsResource(filePath);
-			Debug.Log(LoadedItem);
-			currItem = JsonUtility.FromJson<Item>(LoadedItem);  // parse into item type
-			Debug.Log(currItem.item_Name);
+			//string filePath = "Items/Fire.json";
+			//string LoadedItem = JsonFileReader.LoadJsonAsResource(filePath);
+			//Debug.Log(LoadedItem);
+			//currItem = JsonUtility.FromJson<Item>(LoadedItem);  // parse into item type
+			//Debug.Log(currItem.item_Name);
 	}
 	// Use this for initialization
 	void Start () {
-		tilePosition.x = currItem.item_PosX;
-		tilePosition.y = currItem.item_PosY;
-		tileWorld.SetTile(tilePosition, tileBase);
+		//tilePosition.x = currItem.item_PosX;
+		//tilePosition.y = currItem.item_PosY;
+		//tileWorld.SetTile(tilePosition, tileBase);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		int numtiles = tileWorld.GetUsedTilesCount();
-		Debug.Log("Number of tiles");
-		Debug.Log(numtiles);	
+
+    public void MakeTile (Vector3Int cell)
+    {
+        Debug.Log("In MakeTile\n"); 
+        tileWorld.SetTile(cell, tileBase); 
+    }
+
+    // Update is called once per frame
+    void Update () {
+		//int numtiles = tileWorld.GetUsedTilesCount();
+		//Debug.Log("Number of tiles");
+		//Debug.Log(numtiles);	
 	}
 }
