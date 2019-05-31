0f85078bce99f7e22486f33a772a9f2f5336719b
Alice <wicalice@gmail.com>
2019-05-30 21:29:44 -0400

    Stuck QR in one corner, playing with Sprite as Cozmo's child

---
+++ gridworld_unity/Assets/Sprite.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite : MonoBehaviour
+{
+
+    public GameObject tracker; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3Int new_pos = new Vector3Int((int)tracker.transform.position.x, 
+                                              (int)tracker.transform.position.y,
+                                              (int)tracker.transform.position.z + 1);
+        transform.position = new_pos; 
+     }
+}
