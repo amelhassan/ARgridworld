669615051695275220096d4ae0a1014ef80993ba
Catherine Ding <catherineding28@gmail.com>
2019-05-31 14:49:46 -0400

    Tried to make button to change scenes
    - problem with loading the button in the AR camera
    - maybe have to incorporate the AR camera with the use of the button in order for it to work
    - figure out a way to open button screen first to select an AR scene without using the camera

---
+++ gridworld_unity/Assets/Scripts/menuStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuStart : MonoBehaviour
+{
+    private void Start()
+    {
+        void changemenuscene(string scenename)
+        {
+            SceneManager.LoadScene(scenename);
+        }
+    }
+}
