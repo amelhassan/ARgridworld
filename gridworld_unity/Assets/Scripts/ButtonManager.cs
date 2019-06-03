56f2fc1839c6f4e7b56c013de81a1e5da48ef501
Catherine Ding <catherineding28@gmail.com>
2019-06-03 15:14:13 -0400

    Tried to make AR UI button

---
+++ gridworld_unity/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.IO;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
